352e097b517dd21c1804f7ae67e1fe82de6a7cbc
Daniel Gredler <daniel.gredler@gmail.com>
2015-06-11 21:38:47 -0400

    Fix illegal JavaDoc failing the build.

--- src/main/java/uk/org/okapibarcode/backend/UspsPackage.java
+++ src/main/java/uk/org/okapibarcode/backend/UspsPackage.java
@@ -19,43 +19,44 @@ package uk.org.okapibarcode.backend;
  * USPS Intelligent Mail Package Barcode (IMpb)<br>
  * A linear barcode based on GS1-128. Includes additional data checks.
  * Specification at https://ribbs.usps.gov/intelligentmail_package/documents/tech_guides/BarcodePackageIMSpec.pdf
- * @author Robin Stuart <rstuart114@gmail.com>
+ *
+ * @author <a href="mailto:rstuart114@gmail.com">Robin Stuart</a>
  */
 public class UspsPackage extends Symbol{
-    
+
     @Override
     public boolean encode() {
         String hrt;
         String spacedHrt;
         boolean fourTwenty = false;
         int bracketCount = 0;
-        
+
         if (debug) {
             System.out.printf("IM Package Data Content = \"%s\"\n", content);
         }
-        
+
         if (!(content.matches("[0-9\\[\\]]+"))) {
             /* Input must be numeric only */
             error_msg = "Invalid IMpd data";
             return false;
         }
-        
+
         if ((content.length() % 2) != 0) {
             /* Input must be even length */
             error_msg = "Invalid IMpd data";
             return false;
         }
-        
+
         Code128 code128 = new Code128();
         code128.unsetCc();
         code128.setDataType(Symbol.DataType.GS1);
         code128.setContent(content);
-        
+
         if (content.length() > 4) {
             fourTwenty = ((content.charAt(1) == '4') && (content.charAt(2) == '2') &&
                     (content.charAt(3) == '0'));
         }
-        
+
         hrt = "";
         for (int i = 0; i < content.length(); i++) {
             if (content.charAt(i) == '[') {
@@ -67,7 +68,7 @@ public class UspsPackage extends Symbol{
                 }
             }
         }
-        
+
         spacedHrt = "";
         for(int i = 0; i < hrt.length(); i++) {
             spacedHrt += hrt.charAt(i);
@@ -75,7 +76,7 @@ public class UspsPackage extends Symbol{
                 spacedHrt += " ";
             }
         }
-        
+
         readable = spacedHrt;
         pattern = new String[1];
         pattern[0] = code128.pattern[0];
@@ -83,7 +84,7 @@ public class UspsPackage extends Symbol{
         row_height = new int[1];
         row_height[0] = -1;
         plotSymbol();
-        
+
         return true;
     }
 }

db0562911c0335706dac9de485226195190079ea
Faisal Hameed <faisal.hameed@devfactory.com>
2016-03-07 16:19:47 +0500

    Fixing squid:S2147 - Catches should be combined.

--- src/main/java/uk/org/okapibarcode/gui/OkapiUI.java
+++ src/main/java/uk/org/okapibarcode/gui/OkapiUI.java
@@ -2097,11 +2097,7 @@ public class OkapiUI extends javax.swing.JFrame implements TreeSelectionListener
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(OkapiUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(OkapiUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        } catch (ClassNotFoundException|InstantiationException|IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(OkapiUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(OkapiUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);

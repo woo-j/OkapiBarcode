691760984062d2276f45faa3cda8be3a60a34275
Daniel Gredler <daniel.gredler@gmail.com>
2018-02-13 00:16:38 -0500

    KIX Code: make constants static + final, other misc. Javaisms

--- src/main/java/uk/org/okapibarcode/backend/KixCode.java
+++ src/main/java/uk/org/okapibarcode/backend/KixCode.java
@@ -19,25 +19,26 @@ import java.awt.geom.Rectangle2D;
 import java.util.Locale;
 
 /**
- * Implements Dutch Post KIX Code as used by Royal Dutch TPG Post
- * (Netherlands). Input data can consist of digits 0-9 and characters A-Z.
- * Input should be 11 characters in length. No check digit is added.
+ * <p>Implements Dutch Post KIX Code as used by Royal Dutch TPG Post (Netherlands).
+ *
+ * <p>The input data can consist of digits 0-9 and characters A-Z, and should be 11
+ * characters in length. No check digit is added.
+ *
+ * <p>KIX Code is the same as RM4SCC, but without the check digit.
+ *
  * @author <a href="mailto:rstuart114@gmail.com">Robin Stuart</a>
+ * @see <a href="KIX Code Specification">http://www.tntpost.nl/zakelijk/klantenservice/downloads/kIX_code/download.aspx</a>
  */
 public class KixCode extends Symbol {
 
-    /* Handles Dutch Post TNT KIX symbols */
-    /* The same as RM4SCC but without check digit */
-    /* Specification at http://www.tntpost.nl/zakelijk/klantenservice/downloads/kIX_code/download.aspx */
-
-    private String[] RoyalTable = {
+    private static final String[] ROYAL_TABLE = {
         "TTFF", "TDAF", "TDFA", "DTAF", "DTFA", "DDAA", "TADF", "TFTF", "TFDA",
         "DATF", "DADA", "DFTA", "TAFD", "TFAD", "TFFT", "DAAD", "DAFT", "DFAT",
         "ATDF", "ADTF", "ADDA", "FTTF", "FTDA", "FDTA", "ATFD", "ADAD", "ADFT",
         "FTAD", "FTFT", "FDAT", "AADD", "AFTD", "AFDT", "FATD", "FADT", "FFTT"
     };
 
-    private char[] krSet = {
+    private static final char[] KR_SET = {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
         'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
@@ -45,31 +46,30 @@ public class KixCode extends Symbol {
 
     @Override
     public boolean encode() {
-        String dest;
-        int i;
 
         content = content.toUpperCase(Locale.ENGLISH);
 
-        if(!(content.matches("[0-9A-Z]+"))) {
+        if(!content.matches("[0-9A-Z]+")) {
             error_msg = "Invalid characters in data";
             return false;
         }
 
-        dest = "";
-
-        for (i = 0; i < content.length(); i++) {
-            dest += RoyalTable[positionOf(content.charAt(i), krSet)];
+        StringBuilder sb = new StringBuilder(content.length());
+        for (int i = 0; i < content.length(); i++) {
+            int j = positionOf(content.charAt(i), KR_SET);
+            sb.append(ROYAL_TABLE[j]);
         }
 
+        String dest = sb.toString();
         encodeInfo += "Encoding: " + dest + "\n";
 
         readable = "";
-        pattern = new String[1];
-        pattern[0] = dest;
+        pattern = new String[] { dest };
         row_count = 1;
-        row_height = new int[1];
-        row_height[0] = -1;
+        row_height = new int[] { -1 };
+
         plotSymbol();
+
         return true;
     }
 
@@ -85,27 +85,24 @@ public class KixCode extends Symbol {
         h = 0;
         for (xBlock = 0; xBlock < pattern[0].length(); xBlock++) {
             switch (pattern[0].charAt(xBlock)) {
-            case 'A':
-                y = 0;
-                h = 5;
-                break;
-            case 'D':
-                y = 3;
-                h = 5;
-                break;
-            case 'F':
-                y = 0;
-                h = 8;
-                break;
-            case 'T':
-                y = 3;
-                h = 2;
-                break;
+                case 'A':
+                    y = 0;
+                    h = 5;
+                    break;
+                case 'D':
+                    y = 3;
+                    h = 5;
+                    break;
+                case 'F':
+                    y = 0;
+                    h = 8;
+                    break;
+                case 'T':
+                    y = 3;
+                    h = 2;
+                    break;
             }
-
-            Rectangle2D.Double rect = new Rectangle2D.Double(x, y, w, h);
-            rectangles.add(rect);
-
+            rectangles.add(new Rectangle2D.Double(x, y, w, h));
             x += 2;
         }
         symbol_width = pattern[0].length() * 3;

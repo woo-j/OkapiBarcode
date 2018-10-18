c91a307709cc44d3ed673e9c538354f6d7093db0
Daniel Gredler <daniel.gredler@gmail.com>
2018-10-17 21:24:28 -0400

    Small Aztec Rune cleanup (no functional changes)

--- src/main/java/uk/org/okapibarcode/backend/AztecRune.java
+++ src/main/java/uk/org/okapibarcode/backend/AztecRune.java
@@ -16,11 +16,9 @@
 package uk.org.okapibarcode.backend;
 
 /**
- * Implements Aztec Runes bar code symbology
- * According to ISO/IEC 24778:2008 Annex A
- * <p>
- * Aztec Runes is a fixed-size matrix symbology which can encode whole
- * integer values between 0 and 255.
+ * <p>Implements the Aztec Runes bar code symbology according to ISO/IEC 24778:2008 Annex A.
+ *
+ * <p>Aztec Runes is a fixed-size matrix symbology which can encode whole integer values between 0 and 255.
  *
  * @author <a href="mailto:rstuart114@gmail.com">Robin Stuart</a>
  */
@@ -42,57 +40,47 @@ public class AztecRune extends Symbol {
 
     @Override
     protected void encode() {
-        int decimalValue = 0;
-        int i;
-        int row;
-        int column;
-        String binaryDataStream;
-        String reversedBinaryDataStream;
-        int[] dataCodeword = new int[3];
-        int[] errorCorrectionCodeword = new int[6];
-        ReedSolomon rs = new ReedSolomon();
-        String rowBinary;
-
-        if (content.length() > 3) {
-            throw new OkapiException("Input too large");
-        }
 
         if (!content.matches("[0-9]+")) {
             throw new OkapiException("Invalid input data");
         }
 
+        int decimalValue = 0;
         switch (content.length()) {
-        case 3:
-            decimalValue = 100 * (content.charAt(0) - '0');
-            decimalValue += 10 * (content.charAt(1) - '0');
-            decimalValue += (content.charAt(2) - '0');
-            break;
-        case 2:
-            decimalValue = 10 * (content.charAt(0) - '0');
-            decimalValue += (content.charAt(1) - '0');
-            break;
-        case 1:
-            decimalValue = (content.charAt(0) - '0');
-            break;
+            case 1:
+                decimalValue = (content.charAt(0) - '0');
+                break;
+            case 2:
+                decimalValue = 10 * (content.charAt(0) - '0');
+                decimalValue += (content.charAt(1) - '0');
+                break;
+            case 3:
+                decimalValue = 100 * (content.charAt(0) - '0');
+                decimalValue += 10 * (content.charAt(1) - '0');
+                decimalValue += (content.charAt(2) - '0');
+                break;
+            default:
+                throw new OkapiException("Input too large");
         }
 
         if (decimalValue > 255) {
             throw new OkapiException("Input too large");
         }
 
-        binaryDataStream = "";
-        for (i = 0x80; i > 0; i = i >> 1) {
+        StringBuilder binaryDataStream = new StringBuilder(28);
+        for (int i = 0x80; i > 0; i = i >> 1) {
             if ((decimalValue & i) != 0) {
-                binaryDataStream += "1";
+                binaryDataStream.append('1');
             } else {
-                binaryDataStream += "0";
+                binaryDataStream.append('0');
             }
         }
 
+        int[] dataCodeword = new int[3];
         dataCodeword[0] = 0;
         dataCodeword[1] = 0;
 
-        for (i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             if (binaryDataStream.charAt(i * 4) == '1') {
                 dataCodeword[i] += 8;
             }
@@ -107,72 +95,75 @@ public class AztecRune extends Symbol {
             }
         }
 
+        int[] errorCorrectionCodeword = new int[6];
+
+        ReedSolomon rs = new ReedSolomon();
         rs.init_gf(0x13);
         rs.init_code(5, 1);
         rs.encode(2, dataCodeword);
 
-        for (i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             errorCorrectionCodeword[i] = rs.getResult(i);
         }
 
-        for (i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             if ((errorCorrectionCodeword[4 - i] & 0x08) != 0) {
-                binaryDataStream += '1';
+                binaryDataStream.append('1');
             } else {
-                binaryDataStream += '0';
+                binaryDataStream.append('0');
             }
             if ((errorCorrectionCodeword[4 - i] & 0x04) != 0) {
-                binaryDataStream += '1';
+                binaryDataStream.append('1');
             } else {
-                binaryDataStream += '0';
+                binaryDataStream.append('0');
             }
             if ((errorCorrectionCodeword[4 - i] & 0x02) != 0) {
-                binaryDataStream += '1';
+                binaryDataStream.append('1');
             } else {
-                binaryDataStream += '0';
+                binaryDataStream.append('0');
             }
             if ((errorCorrectionCodeword[4 - i] & 0x01) != 0) {
-                binaryDataStream += '1';
+                binaryDataStream.append('1');
             } else {
-                binaryDataStream += '0';
+                binaryDataStream.append('0');
             }
         }
 
-        reversedBinaryDataStream = "";
-        for (i = 0; i < binaryDataStream.length(); i++) {
+        StringBuilder reversedBinaryDataStream = new StringBuilder(28);
+        for (int i = 0; i < binaryDataStream.length(); i++) {
             if ((i & 1) == 0) {
                 if (binaryDataStream.charAt(i) == '0') {
-                    reversedBinaryDataStream += "1";
+                    reversedBinaryDataStream.append('1');
                 } else {
-                    reversedBinaryDataStream += "0";
+                    reversedBinaryDataStream.append('0');
                 }
             } else {
-                reversedBinaryDataStream += binaryDataStream.charAt(i);
+                reversedBinaryDataStream.append(binaryDataStream.charAt(i));
             }
         }
 
         encodeInfo += "Binary: " + reversedBinaryDataStream + "\n";
 
-        rowBinary = "";
         readable = "";
         pattern = new String[11];
         row_count = 11;
         row_height = new int[11];
-        for (row = 0; row < 11; row++) {
-            for (column = 0; column < 11; column++) {
+
+        for (int row = 0; row < 11; row++) {
+            StringBuilder rowBinary = new StringBuilder(11);
+            for (int column = 0; column < 11; column++) {
                 if (BIT_PLACEMENT_MAP[(row * 11) + column] == 1) {
-                    rowBinary += "1";
+                    rowBinary.append('1');
                 }
                 if (BIT_PLACEMENT_MAP[(row * 11) + column] == 0) {
-                    rowBinary += "0";
+                    rowBinary.append('0');
                 }
                 if (BIT_PLACEMENT_MAP[(row * 11) + column] >= 2) {
-                    rowBinary += reversedBinaryDataStream.charAt(BIT_PLACEMENT_MAP[(row * 11) + column] - 2);
+                    rowBinary.append(reversedBinaryDataStream.charAt(BIT_PLACEMENT_MAP[(row * 11) + column] - 2));
                 }
             }
-            pattern[row] = bin2pat(rowBinary);
+            pattern[row] = bin2pat(rowBinary.toString());
             row_height[row] = 1;
-            rowBinary = "";
         }
     }
 }

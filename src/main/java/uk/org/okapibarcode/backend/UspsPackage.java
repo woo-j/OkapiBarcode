b2caad961913109d349c8938bc824e479f1f968e
Daniel Gredler <daniel.gredler@gmail.com>
2022-07-17 19:19:52 -0400

    Add more tests for USPS Package

--- src/main/java/uk/org/okapibarcode/backend/UspsPackage.java
+++ src/main/java/uk/org/okapibarcode/backend/UspsPackage.java
@@ -44,68 +44,54 @@ public class UspsPackage extends Symbol {
         code128.setDataType(DataType.GS1);
         code128.setContent(content);
 
-        boolean fourTwenty = content.length() > 4 &&
+        boolean fourTwenty = content.length() >= 5 &&
+                             content.charAt(0) == '[' &&
                              content.charAt(1) == '4' &&
                              content.charAt(2) == '2' &&
-                             content.charAt(3) == '0';
+                             content.charAt(3) == '0' &&
+                             content.charAt(4) == ']';
 
-        String hrt = "";
-        int bracketCount = 0;
-        for (int i = 0; i < content.length(); i++) {
-            if (content.charAt(i) == '[') {
-                bracketCount++;
-            }
-            if (!(fourTwenty && bracketCount < 2)) {
-                if ((content.charAt(i) >= '0') && (content.charAt(i) <= '9')) {
-                    hrt += content.charAt(i);
+        StringBuilder hrt = new StringBuilder();
+        int start = (fourTwenty ? content.indexOf('[', 5) : 0);
+        if (start != -1) {
+            int digits = 0;
+            for (int i = start; i < content.length(); i++) {
+                char c = content.charAt(i);
+                if (c >= '0' && c <= '9') {
+                    hrt.append(c);
+                    digits++;
+                    if (digits % 4 == 0) {
+                        hrt.append(' ');
+                    }
                 }
-            }
-        }
-
-        String spacedHrt = "";
-        for(int i = 0; i < hrt.length(); i++) {
-            spacedHrt += hrt.charAt(i);
-            if (i % 4 == 3) {
-                spacedHrt += " ";
             }
         }
 
         encodeInfo = code128.encodeInfo;
-        readable = spacedHrt;
-        pattern = new String[1];
-        pattern[0] = code128.pattern[0];
+        readable = hrt.toString();
+        pattern = new String[] { code128.pattern[0] };
         row_count = 1;
-        row_height = new int[1];
-        row_height[0] = -1;
+        row_height = new int[] { -1 };
     }
 
     @Override
     protected void plotSymbol() {
-        int xBlock;
-        int x, y, w, h;
-        boolean black;
-        int offset = 20;
-        int yoffset = 15;
-        String banner = "USPS TRACKING #";
 
         rectangles.clear();
         texts.clear();
-        y = yoffset;
-        h = 0;
-        black = true;
-        x = 0;
-        for (xBlock = 0; xBlock < pattern[0].length(); xBlock++) {
-            w = pattern[0].charAt(xBlock) - '0';
+
+        int offset = 20;
+        int yoffset = 15;
+        int x = 0;
+        int y = yoffset;
+        int h = default_height;
+        boolean black = true;
+
+        for (int xBlock = 0; xBlock < pattern[0].length(); xBlock++) {
+            int w = pattern[0].charAt(xBlock) - '0';
             if (black) {
-                if (row_height[0] == -1) {
-                    h = default_height;
-                } else {
-                    h = row_height[0];
-                }
-                if (w != 0 && h != 0) {
-                    Rectangle2D.Double rect = new Rectangle2D.Double(x + offset, y, w, h);
-                    rectangles.add(rect);
-                }
+                Rectangle2D.Double rect = new Rectangle2D.Double(x + offset, y, w, h);
+                rectangles.add(rect);
                 symbol_width = x + w + (2 * offset);
             }
             black = !black;
@@ -113,14 +99,22 @@ public class UspsPackage extends Symbol {
         }
         symbol_height = h + (2 * yoffset);
 
-        // Add boundary bars
+        // add boundary bars
         Rectangle2D.Double topBar = new Rectangle2D.Double(0, 0, symbol_width, 2);
         Rectangle2D.Double bottomBar = new Rectangle2D.Double(0, symbol_height - 2, symbol_width, 2);
         rectangles.add(topBar);
         rectangles.add(bottomBar);
 
+        // add banner and human-readable text
+        texts.add(new TextBox(0, 12.0, symbol_width, "USPS TRACKING #", humanReadableAlignment));
         texts.add(new TextBox(0, symbol_height - 6.0, symbol_width, readable, humanReadableAlignment));
-        texts.add(new TextBox(0, 12.0, symbol_width, banner, humanReadableAlignment));
+    }
+
+    @Override
+    public int getHeight() {
+        // because of the custom layout logic in plotSymbol(), we store all height components (e.g. human
+        // readable text height, quiet zone, boundary bars) here; this is not the case for most symbologies
+        return symbol_height;
     }
 
     /** {@inheritDoc} */

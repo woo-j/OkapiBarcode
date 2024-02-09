ab44e556f99e5ff3942a882194e880c96d9be47d
Daniel Gredler <daniel.gredler@gmail.com>
2024-02-09 12:37:41 +0100

    Remove unnecessary int array zero init, fix line endings

--- src/main/java/uk/org/okapibarcode/backend/ReedSolomon.java
+++ src/main/java/uk/org/okapibarcode/backend/ReedSolomon.java
@@ -17,38 +17,31 @@
 package uk.org.okapibarcode.backend;
 
 /**
- *
  * @author <a href="mailto:rstuart114@gmail.com">Robin Stuart</a>
  */
 public class ReedSolomon {
+
     private int logmod;
     private int rlen;
-
     private int[] logt;
     private int[] alog;
     private int[] rspoly;
-    public int[] res;
-
-    public int getResult(int count) {
-        return res[count];
-    }
+    private int[] res;
 
     public void init_gf(int poly) {
-        int m, b, p, v;
-
         // Find the top bit, and hence the symbol size
+        int m, b;
         for (b = 1, m = 0; b <= poly; b <<= 1) {
             m++;
         }
         b >>= 1;
         m--;
-
-        // Calculate the log/alog tables
+        // Calculate the log / alog tables
         logmod = (1 << m) - 1;
         logt = new int[logmod + 1];
         alog = new int[logmod];
 
-        for (p = 1, v = 0; v < logmod; v++) {
+        for (int p = 1, v = 0; v < logmod; v++) {
             alog[v] = p;
             logt[p] = v;
             p <<= 1;
@@ -59,16 +52,12 @@ public class ReedSolomon {
     }
 
     public void init_code(int nsym, int index) {
-        int i, k;
-
-        rspoly = new int[nsym + 1];
-
         rlen = nsym;
-
+        rspoly = new int[nsym + 1];
         rspoly[0] = 1;
-        for (i = 1; i <= nsym; i++) {
+        for (int i = 1; i <= nsym; i++) {
             rspoly[i] = 1;
-            for (k = i - 1; k > 0; k--) {
+            for (int k = i - 1; k > 0; k--) {
                 if (rspoly[k] != 0) {
                     rspoly[k] = alog[(logt[rspoly[k]] + index) % logmod];
                 }
@@ -80,27 +69,25 @@ public class ReedSolomon {
     }
 
     public void encode(int len, int[] data) {
-        int i, k, m;
-
         res = new int[rlen];
-        for (i = 0; i < rlen; i++) {
-            res[i] = 0;
-        }
-
-        for (i = 0; i < len; i++) {
-            m = res[rlen - 1] ^ data[i];
-            for (k = rlen - 1; k > 0; k--) {
-                if ((m != 0) && (rspoly[k] != 0)) {
+        for (int i = 0; i < len; i++) {
+            int m = res[rlen - 1] ^ data[i];
+            for (int k = rlen - 1; k > 0; k--) {
+                if (m != 0 && rspoly[k] != 0) {
                     res[k] = res[k - 1] ^ alog[(logt[m] + logt[rspoly[k]]) % logmod];
                 } else {
                     res[k] = res[k - 1];
                 }
             }
-            if ((m != 0) && (rspoly[0] != 0)) {
+            if (m != 0 && rspoly[0] != 0) {
                 res[0] = alog[(logt[m] + logt[rspoly[0]]) % logmod];
             } else {
                 res[0] = 0;
             }
         }
+    }
+
+    public int getResult(int count) {
+        return res[count];
     }
 }

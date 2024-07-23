ada19e3a4bf63215fb8558f4824750e927f9266d
Daniel Gredler <daniel.gredler@gmail.com>
2024-07-23 13:59:37 +0200

    PR #111 small cleanup

--- src/test/java/uk/org/okapibarcode/output/SvgRendererTest.java
+++ src/test/java/uk/org/okapibarcode/output/SvgRendererTest.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Locale;
 
 import org.junit.jupiter.api.AfterEach;
